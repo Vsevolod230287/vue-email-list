f3174dc629b8959d573a359909178ad7f819b8c2
Vsevolod <cerneavschivsevolod@gmail.com>
2021-04-09 09:59:48 +0200

    Bonus: compaiono tutti 10 insieme le email

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ var app = new Vue({
       for (let i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
           .then((response) => {
-            let email = response.data.response
-            this.emails.push(email)
+            this.emails.push(response.data.response)
           })
       }
 
